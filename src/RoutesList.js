7afdf5a6003d3eacd373ff2577f562d292012c3c
Elise Ngo <elisetvy@gmail.com>
2023-10-03 15:48:05 -0700

    Create components

---
+++ src/RoutesList.js
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./Homepage";
+import CompanyList from "./CompanyList";
+import CompanyDetail from "./CompanyDetail";
+import JobList from "./JobList";
+
+function RoutesList() {
+  return (
+    <Routes>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/companies" element={<CompanyList />} />
+      <Route path="/companies/:handle" element={<CompanyDetail />} />
+      <Route path="/jobs" element={<JobList />} />
+    </Routes>
+  )
+}
+
+export default RoutesList;
