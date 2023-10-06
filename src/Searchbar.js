970b3d2e71914c7f5b223925a50ec99d8e3a653d
Daniel Zych <danjzych@gmail.com>
2023-10-06 18:18:41 -0500

    Added a few style tweaks and tweak to the live search

--- src/Searchbar.js
+++ src/Searchbar.js
@@ -1,5 +1,5 @@
 import { useState, useCallback } from "react";
-import debounce from "lodash.debounce"
+import debounce from "lodash.debounce";
 import "./Searchbar.css";
 
 /** Reusable searchbar component for Jobly App.
@@ -16,21 +16,22 @@ function Searchbar({ searchType, search }) {
   const [formData, setFormData] = useState("");
 
   /**Executes search function after 500ms delay */
-  const debouncedFetchData = useCallback(debounce((formData) => {
-    search(formData);
-  }, 500), []);
+  const debouncedFetchData = useCallback(
+    debounce((formData) => {
+      search(formData);
+    }, 500),
+    []
+  );
 
   /**Update state based on change in form input */
   function handleChange(evt) {
     setFormData(evt.target.value);
-    debouncedFetchData(evt.target.value, 500);
+    debouncedFetchData(evt.target.value.trim(), 500);
   }
 
-  /**Invokes search function on form submission */
+  /**Prevents page from reloading on submission */
   function handleSubmit(evt) {
     evt.preventDefault();
-    search(formData.trim());
-    setFormData("");
   }
 
   return (
